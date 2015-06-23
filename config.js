910df8676c5210db7e09d4979322b282887ba07e
MMxM <mmedeiros450@gmail.com>
2015-06-23 15:40:38 -0300

    nova postagem

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ var file_list = [
 
 
  ["/home/mmxm", 0,"README", "2015 Jun 22 17:00","Meu sitezim v0.0.1<br/>",""],
- ["/home/mmxm", 0,"links.txt", "2015 Jun 22 17:00","Alguns links legais :D<br/>",""]
+ ["/home/mmxm", 0,"links.txt", "2015 Jun 22 17:00","Alguns links legais :D<br/>",""],
+
+ ["/home/mmxm/textos", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://raw.githubusercontent.com/P0cL4bs/Shellcodes/master/read_file_tutorial.txt"]
 
 ];
 
